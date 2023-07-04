f0d43c11992783df8204a74af1c7f4ad771da814
chris901003 <a0987999103@gmail.com>
2023-07-04 19:10:50 +0800

    Initial Commit

---
+++ GithubRepositoryApp/GithubRepositoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepositoryAppApp.swift
+//  GithubRepositoryApp
+//
+//  Created by 黃弘諺 on 2023/7/4.
+//
+
+import SwiftUI
+
+@main
+struct GithubRepositoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
